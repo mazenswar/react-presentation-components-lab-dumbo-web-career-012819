27a75f2c447be7533b0543302c2d955dd7c17792
mazenswar <mazenswar@protonmail.com>
2019-04-08 16:24:00 -0400

    react presentational components

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,19 @@
 // Code SimpleComponent Here
+import React from 'react'
+export default class SimpleComponent extends React.Component {
+  state = {
+    mood: 'happy'
+  }
+
+  handleClick = () => {
+    let newMood = this.state.mood === 'happy' ? 'sad' : 'happy'
+    this.setState({ mood: newMood })
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    )
+  }
+
+}
